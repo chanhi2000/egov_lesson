29afa431dbdecf9fce081b42457a98a5d59e458c
chanhi2000 <chanhi2000@gmail.com>
2019-03-23 20:58:07 +0900

    lesson03 corrected jstl setup

--- lesson03E/src/main/java/com/hr/HelloController.java
+++ lesson03E/src/main/java/com/hr/HelloController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-
     private Logger LOG = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("main/main.do")
